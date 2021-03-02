97d905fc32ab22f22b05a17053937f0d8429b9ea
Milan <milanvpage@gmail.com>
2021-03-02 13:38:32 -0800

    commit tree 63cf2528fe91c280572cbe23e86322f520ee19ff

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
-
-def countdown
+def countdown(x)
+    while x >= 1
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+ 
+def countdown_with_sleep(x)
+    while x >= 1
+        puts "#{x} SECOND(S)!"
+        sleep(1)
+        x -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+
+
+
